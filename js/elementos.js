6f3d02e97e0e16421c9c68b91a8cf99ad46e25db
pandaj <mail@juliocofre.cl>
2013-10-21 11:43:38 -0400

    PokeElem 0.01
    Eso

---
+++ js/elementos.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+var elementos = new Array();
+
+elementos[0] = new Object();
+elementos[0].nombre = 'Acero';
+elementos[0].icono = 'acero.png';
+elementos[0].icono = 'acero.png';
